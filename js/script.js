f0409bc4621c07b6f6341fd4e865f2e3c04b4b14
1uanss <sousasluan212@gmail.com>
2026-03-09 21:21:39 -0300

    projeto em estagio final, js esta em andamento

--- js/script.js
+++ js/script.js
@@ -3,8 +3,19 @@ const personagemNumber = document.querySelector('.personagem_number')
 const personagemImage = document.querySelector('.personagem_image')
 const searchInput = document.querySelector('.search_input')
 
+
+
 const fetchNaruto = async (naruto) => {
-    const APIresponse = await fetch(`https://dattebayo-api.onrender.com/characters?name=${naruto}`)
+    // const APIresponse = await fetch(`https://dattebayo-api.onrender.com/characters?name=${naruto}`)
+
+    let url = ""
+
+    if(!isNaN(naruto)){
+        url = `https://dattebayo-api.onrender.com/characters/${naruto}`
+    }else{
+        url = `https://dattebayo-api.onrender.com/characters?name=${naruto}`
+    }
+    const APIresponse = await fetch(url)
 
     if(APIresponse.status === 200){
         const data = await APIresponse.json()
@@ -17,17 +28,33 @@ const fetchNaruto = async (naruto) => {
 const renderNaruto = async (naruto) => {
     const data = await fetchNaruto(naruto)
     if(data){
-        personagemName.innerHTML = data['characters']['0']['name']
-        personagemNumber.innerHTML = data['characters']['0']['id']
-        personagemImage.src = data['characters']['0']['images'][0]
-        searchInput.value = ""
+        personagemName.innerHTML = 'Loading...'
+        personagemNumber.innerHTML = '' 
+        // personagemImage.style.display = "none"
+        
+        if(data.characters){
+            //busca pelo nome
+            personagemName.innerHTML = data['characters'][0]['name']
+            personagemNumber.innerHTML = data['characters'][0]['id']
+            personagemImage.src = data['characters'][0]['images'][0]
+            searchInput.value = ""
+        }else{
+            //busca peo id
+            personagemImage.src = data['images'][0]
+            personagemName.innerHTML = data.id
+            personagemNumber.innerHTML = data.name
+            searchInput.value = ""
+        }
+        
     }else{
         personagemName.innerHTML = "Nao encontrado"
         personagemNumber.innerHTML = ""
-        personagemImage.src = ""
+        personagemImage.src = "none"
     }
 
 }
-// fetchNaruto(1)
-renderNaruto('Naruto Uzumaki')
 
+addEventListener('submit', (evente)=>{
+    evente.preventDefault()
+    renderNaruto(searchInput.value)
+})
